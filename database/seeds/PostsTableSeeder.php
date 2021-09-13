128990ebc5d1ad6541494c140f0b4d2e71e4b0a4
Piero <piero.onorati@virgilio.it>
2021-09-13 17:47:05 +0200

    make seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostsTableSeeder extends Seeder
@@ -12,8 +13,18 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i < ; $i++) { 
-            # code...
+       for ($i=0; $i < 10 ; $i++) { 
+
+            //creare l'istanza
+            $newPost = new Post();
+
+            // generare i dati
+            $newPost->title= "Post numero" . ($i+1);
+            $newPost->slug= Str::slug($newPost->title, '-');
+            $newPost->content ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, suscipit! Quibusdam dolorum tenetur, repellat dolores aspernatur vel quasi nisi doloremque, veniam, fugiat maiores modi neque. Debitis libero pariatur optio quod?';
+
+            // salvare i dati
+            $newPost->save();
         }
     }
 }
